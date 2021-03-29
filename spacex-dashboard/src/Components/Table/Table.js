8772c30aa27701cd18ac8f1d653e453051d39608
Shubham Sinha <shubhamsinha.cer18@itbhu.ac.in>
2021-03-30 05:27:05 +0530

    Added Loading animation and detail popup modal

--- spacex-dashboard/src/Components/Table/Table.js
+++ spacex-dashboard/src/Components/Table/Table.js
@@ -4,6 +4,8 @@ import "./Table.css";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import LoadingContainer from "../../loadingAnimation/rocketAnimation";
+import api from "../../api";
+import Modal from "@material-ui/core/Modal";
 
 const columns = [
   {
@@ -49,7 +51,7 @@ const columns = [
   {
     field: "launchstatus",
     headerName: "Launch Status",
-    width: 160,
+    width: 130,
     sortable: false,
     headerClassName: "customColumn launch-status",
     headerAlign: "left",
@@ -77,6 +79,11 @@ const useStyles = makeStyles({
       fontWeight: "600",
       borderRadius: "200px",
       textAlign: "center",
+      position: "relative !important",
+      top: "6px !important",
+      lineHeight: "40px !important",
+      minHeight: "8px !important",
+      maxHeight: "40px !important",
     },
     "& .super-app.Failure": {
       backgroundColor: "#FDE2E1",
@@ -84,6 +91,11 @@ const useStyles = makeStyles({
       fontWeight: "600",
       borderRadius: "200px",
       textAlign: "center",
+      position: "relative !important",
+      top: "6px !important",
+      lineHeight: "40px !important",
+      minHeight: "8px !important",
+      maxHeight: "40px !important",
     },
     "& .super-app.Upcoming": {
       backgroundColor: "#FEF3C7",
@@ -91,16 +103,37 @@ const useStyles = makeStyles({
       fontWeight: "600",
       borderRadius: "200px",
       textAlign: "center",
+      position: "relative !important",
+      top: "6px !important",
+      lineHeight: "40px !important",
+      minHeight: "8px !important",
+      maxHeight: "40px !important",
     },
   },
 });
 
+const useDetailStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    top: 50,
+    left: 50,
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 const DataTable = ({ data }) => {
   const classes = useStyles();
+  const classesDetail = useDetailStyles();
+  const [open, setOpen] = React.useState(false);
+  const [flightData, setflightData] = React.useState({});
   const rows = data.map((item, index) => {
     let data = {
       id: index + 1,
-      launched: item.launch_date_local,
+      launched: item.launch_date_utc,
       location: item.launch_site.site_name,
       mission: item.mission_name,
       orbit: item.rocket.second_stage.payloads[0].orbit,
@@ -113,17 +146,121 @@ const DataTable = ({ data }) => {
     };
     return data;
   });
+
+  const CustomLoader = () => {
+    return (
+      <div className="div-loading">
+        <LoadingContainer />
+      </div>
+    );
+  };
+  const clickHandler = async (id) => {
+    setOpen(true);
+    let response = await api.get(`/${data[id - 1].flight_number}`);
+    if (response.status === 200) {
+      console.log(response.data);
+      setflightData(response.data);
+    }
+  };
+
   return (
-    <div className="Table" style={{ width: "76.5%" }}>
+    <div className="Table" style={{ width: "74.55%" }}>
       <DataGrid
         disableColumnMenu
         hideFooterSelectedRowCount
         autoHeight
+        autoWidth
         className={classes.root}
         rows={rows}
+        onRowClick={(GridRowData) => clickHandler(GridRowData.id)}
+        components={{
+          LoadingOverlay: CustomLoader,
+        }}
+        loading={rows.length > 0 ? false : true}
         columns={columns}
         pageSize={10}
       />
+      <div className="Detail-Modal">
+        <Modal
+          open={open}
+          onClose={() => {
+            setOpen(!open);
+          }}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          <div className={classesDetail.paper}>
+            <div className="top-section">
+              <img
+                className="top-section-image"
+                src={flightData?.links?.mission_patch_small}
+                alt=" "
+              ></img>
+              <div>
+                <h5>{flightData?.mission_name}</h5>
+                <p>{flightData?.rocket?.rocket_name}</p>
+              </div>
+              <p>{flightData?.launch_success}</p>
+            </div>
+            <div className="detailDescription">
+              <p>
+                {flightData?.details}{" "}
+                <a
+                  href={flightData?.links?.wikipedia}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Wikipedia
+                </a>
+              </p>
+            </div>
+            <div className="lower-section">
+              <div className="flight-row">
+                <p>Flight Number</p>
+                <p>{flightData?.flight_number}</p>
+              </div>
+              <div className="flight-row">
+                <p>Mission Name</p>
+                <p>{flightData?.mission_name}</p>
+              </div>
+              <div className="flight-row">
+                <p>Rocket Type</p>
+                <p>{flightData?.rocket?.rocket_type}</p>
+              </div>
+              <div className="flight-row">
+                <p>Rocket Name</p>
+                <p>{flightData?.rocket?.rocket_name}</p>
+              </div>
+              <div className="flight-row">
+                <p>Manufacturer</p>
+                <p>SpaceX</p>
+              </div>
+              <div className="flight-row">
+                <p>Nationality</p>
+                <p>SpaceX</p>
+              </div>
+              <div className="flight-row">
+                <p>Launch Date</p>
+                <p>{flightData?.launch_date_utc}</p>
+              </div>
+              <div className="flight-row">
+                <p>Payload Type</p>
+                <p>
+                  {flightData?.rocket?.second_stage?.payloads[0]?.payload_type}
+                </p>
+              </div>
+              <div className="flight-row">
+                <p>Orbit</p>
+                <p>{flightData?.rocket?.second_stage?.payloads[0]?.orbit}</p>
+              </div>
+              <div className="flight-row">
+                <p>Launch Site</p>
+                <p>{flightData?.launch_site?.site_name}</p>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      </div>
     </div>
   );
 };
